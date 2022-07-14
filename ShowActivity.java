c4c5dca979b353155f13a5346fde971231903f58
d-n1sh <104403770+d-n1sh@users.noreply.github.com>
2022-07-14 17:42:07 +0800

    Add files via upload

---
+++ ShowActivity.java
@@ -0,0 +1,43 @@
+package com.myapplicationdev.android.ndpsongs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ShowActivity extends AppCompatActivity{
+
+    Button btnFilter;
+    ListView lv;
+    ArrayAdapter<Song> aa;
+    Song data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        lv = findViewById(R.id.lv);
+
+        Intent x = getIntent();
+        data = (Song) x.getSerializableExtra("id");
+
+        aa = new ArrayAdapter<Song>(android.R.layout.simple_list_item_2, al);
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Song data =
+            }
+        });
+
+    }
+}
